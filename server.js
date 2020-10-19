3c610404b0b434bda52e1bfbd006c4c53d52daca
ethan <izawaryosuke0101@gmail.com>
2020-10-19 14:05:56 +0900

    req.url

--- server.js
+++ server.js
@@ -1,12 +1,33 @@
 //httpモジュールを読み込む
+const { read } = require('fs');
 var http = require('http');
+//settings.jsからの読み込み
+var settings = require('./settings.js')
+console.log(settings);
+
 //サーバーを作る
 var server = http.createServer();
+
+var msg;
 //イベントを結びつける
 server.on('request', function(req,res){
+  switch (req.url){
+    case '/about':
+      msg = "about this page";
+      break;
+    case '/profile':
+      msg = "about me";
+      break;
+    default:
+      msg = 'wrong page';
+      break;
+  }
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.write('hello world');
+  res.write(msg);
   res.end();
 });
-server.listen(8000, '127.0.0.1');
+server.listen(settings.port, settings.host);
 console.log("server listening");
+
+
+
